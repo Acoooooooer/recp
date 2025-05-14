5b9107a1751cce28a022bc84405c93c2effd4c76
正在泪奔的阿犬 <1402024943@qq.com>
2025-05-14 09:55:30 +0800

    2

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 
 const router = ['performance', 'reliablity', 'scale']
